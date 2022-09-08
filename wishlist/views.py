0ad2c0aeab28d742329699846fe7e175a19343be
unknown <matthewrizkyhartadi@gmail.com>
2022-09-08 11:24:59 +0700

    Completed Lab01

---
+++ wishlist/views.py
@@ -0,0 +1,13 @@
+from django.shortcuts import render
+from wishlist.models import ItemWishlist
+
+# Create your views here.
+def show_wishlist(request):
+    data_wishlist_item = ItemWishlist.objects.all()
+    context = {
+    'list_item': data_wishlist_item,
+    'name': 'Matthew Rizky Hartadi'
+}
+    return render(request, "wishlist.html", context)
+    
+
